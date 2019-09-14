c0b5dc61293959cfe6a8ede3dc0b2548dffb4953
anujgarg204 <anujgarg204@gmail.com>
2019-09-14 18:12:03 +0530

    second commit

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import Auxiliary from '../../hoc/Auxiliary'
+import React from 'react';
+
+import Aux from '../../hoc/Auxiliary'
+import classes from './Layout.css'
 
 const layout = (props) => {
     return (
-        <Auxiliary>
+        <Aux>
         <div>Toolbar,sideDrawer,Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
-        </Auxiliary>
+        </Aux>
      );
 }
  

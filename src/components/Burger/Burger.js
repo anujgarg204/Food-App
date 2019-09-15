0b4d99f25296a2a9009a04a6b905ed18713e4c8e
anujgarg204 <anujgarg204@gmail.com>
2019-09-15 19:07:15 +0530

    Burger controls design

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,20 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients'
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_,i) => {
-           return <BurgerIngredient key={igKey +1} type={igKey}/>
+           return <BurgerIngredient key={igKey +i} type={igKey}/>
         })
-    })
+    }).reduce((arr,el) => {
+        return arr.concat(el)
+    },[]);
+
+    if (transformedIngredients.length === 0 ){
+        transformedIngredients = <p>How about some ingredients</p>;
+    }
+    
+
     return (
         <div className={classes.Burger}>
                 <BurgerIngredient type="bread-top" />

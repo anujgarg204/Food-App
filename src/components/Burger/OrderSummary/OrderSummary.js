5d0cdc9891da318d3265f2a8a388f2b81a1c3b67
anujgarg204 <anujgarg204@gmail.com>
2019-09-16 15:33:34 +0530

    Toolbar functionality improved

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import Aux from '../../../hoc/Auxiliary'
+import Button from '../../UI/Button/Button'
+
 
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
@@ -15,7 +17,10 @@ const orderSummary = (props) => {
         <ul>
             {ingredientSummary}
         </ul>
+        <p><strong>Your total: {props.price.toFixed(2)}</strong></p>
         <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
     </Aux>
      );
 }

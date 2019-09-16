f32343b989ee971d5500f7729a16efb281e7d771
anujgarg204 <anujgarg204@gmail.com>
2019-09-16 17:04:00 +0530

    Sidedrawer update

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,16 +6,28 @@ import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 
-const layout = (props) => {
-    return (
-        <Aux>
-        <Toolbar/>
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-        </Aux>
-     );
+class Layout extends React.Component{
+ state = {
+    showSideDrawer: true
+}
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+   render(){
+       return (
+           <Aux>
+           <Toolbar/>
+           <SideDrawer  
+           open = {this.state.showSideDrawer}
+           closed={this.sideDrawerClosedHandler}/>
+           <main className={classes.Content}>
+               {this.props.children}
+           </main>
+           </Aux>
+        );
+   }
 }
  
-export default layout;
+export default Layout;

138af306dfe5f4c1fea6a3d1ce2defea443e05ab
anujgarg204 <anujgarg204@gmail.com>
2019-09-18 00:27:26 +0530

    Forms

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import classes from './Input.css'
+
+const input = (props) => {
+  let inputElement = null;
+  
+    switch(props.elementType){
+          case('input'):
+          inputElement = <input className={classes.Input} {...props.elementConfig} value={props.value}/>;
+          break;
+          case('textarea'): 
+          inputElement=<textarea {...props.elementConfig} value={props.value}/>;
+          break;
+          default:
+              inputElement= <input className={classes.Input} {...props.elementConfig} value={props.value}/>
+  }
+
+  return ( <div>
+        <label className={classes.Label}>{props.label}</label>
+        {inputElement}
+    </div> );
+}
+ 
+export default input;

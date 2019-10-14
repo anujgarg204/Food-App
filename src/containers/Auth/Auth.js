5321a7ad974bc3696feb9dd5a41694b7a33d894d
anujgarg204 <anujgarg204@gmail.com>
2019-10-14 18:01:22 +0530

    Auth addition

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+import Input from '../../components/UI/Input/Input'
+import Button from '../../components/UI/Button/Button'
+
+class Auth extends Component {
+    state = {
+        controls: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Mail address'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            }
+        }
+    }
+    render() {
+        const formElementArray = [];
+        for (let key in this.state.orderForm) {
+            formElementArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
+        const form = formElementArray.map(formElement => (
+            <Input
+                key={formElement.id}
+                elementType={formElement.config.elementType}
+                elementConfig={formElement.config.elementConfig}
+                value={formElement.config.value}
+                invalid={!formElement.config.valid}
+                shouldValidate={formElement.config.validation}
+                touched={formElement.config.touched}
+                changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+            <Button />
+                ))
+        return (
+            <div>
+                <form></form>
+            </div>
+        );
+    }
+}
+
+export default Auth;

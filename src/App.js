4bf8df694b294c222924fd065facb0f27a6a0ee7
anujgarg204 <anujgarg204@gmail.com>
2019-10-30 18:35:19 +0530

    environment variables

--- src/App.js
+++ src/App.js
@@ -16,27 +16,28 @@ class App extends Component {
   }
 
   render() {
-   
-    let routes = (
-    <Switch>
-      <Route path="/auth" component={Auth} />
-      <Route path="/" exact component={BurgerBuilder} />
-      <Redirect to="/" />
-    </Switch>)
 
-    if(this.props.isAuthenticated){
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>)
+
+    if (this.props.isAuthenticated) {
       routes = (<Switch>
         <Route path="/checkout" component={Checkout} />
         <Route path="/orders" component={Orders} />
         <Route path="/logout" component={Logout} />
+        <Route path="/auth" component={Auth} />
         <Route path="/" exact component={BurgerBuilder} />
       </Switch>)
     }
-   
+
     return (
       <div>
         <Layout>
-         {routes}
+          {routes}
         </Layout>
       </div>
     );
